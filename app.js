c0d9e237609cfcfebf6c876f160659f54fea8ca4
clevin m.wilson <clevinmwilson@gmail.com>
2020-12-01 11:40:47 +0530

    admin login edited

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var hbs = require('express-handlebars')
 var db=require('./config/connection')
 var session = require('express-session')
+const mongoose = require('mongoose');
 var adminRouter = require('./routes/admin');
 var usersRouter = require('./routes/users');
 
