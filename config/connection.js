ded4a2d49bc3f383b41cd1930d428c0560624f2e
clevin m.wilson <clevinmwilson@gmail.com>
2021-01-10 08:51:29 +0530

    {useUnifiedTopology: true} in connection.js line 9

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ module.exports.connect=function(done){
     const url='mongodb://localhost:27017'
     const dbname='moviebooking'
 
-    mongoClient.connect(url,(err,data)=>{
+    mongoClient.connect(url, {useUnifiedTopology: true},(err,data)=>{
         if(err) return done(err)
         state.db=data.db(dbname)
         done()
