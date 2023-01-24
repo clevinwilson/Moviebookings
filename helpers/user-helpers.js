912d3862cf5f47210ce37b55f4328ce845d9aac4
Clevin <64676900+clevinwilson@users.noreply.github.com>
2023-01-24 20:23:56 +0530

    Update user-helpers.js

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -347,7 +347,7 @@ module.exports = {
                 requireTLS: true,
                 auth: {
                     user: "www.myappsa2z@gmail.com",
-                    pass: "nbkbeaxahwlzmoog",
+                    pass: "",
                 },
             });
 
